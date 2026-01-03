8f7bfa58658380972ab9a554c943010db0b4c5d9
Sujal Vasara <vasarasujal.cg@gmail.com>
2026-01-03 10:29:05 +0530

    feat: add core configuration and main app setup

---
+++ Backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const authRoutes = require('./modules/auth/authRoutes');
+const userRoutes = require('./modules/user/userRoutes');
+const attendanceRoutes = require("./modules/attendanceCopy/attendance.routes");
+const payrollroutes = require('./modules/payroll/payrollroutes');
+const calendar = require('./modules/calendar/holidayRoutes');
+const mailroutes = require('./modules/mail/mailroutes');
+const cloudinaryroutes = require('./modules/cloudinary/cloudinaryroutes');
+const app = express();
+
+// ✅ Configure CORS for React frontend
+app.use(cors());
+
+//  Body parser & cookies
+app.use(express.json());
+app.use(cookieParser());
+
+// ✅ Request logger (good for debugging)
+app.use((req, res, next) => {
+  console.log(`[📥 ${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+// ✅ Mount API routes
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', attendanceRoutes);
+app.use('/api', payrollroutes);
+app.use('/api', calendar);
+app.use('/api', mailroutes);
+app.use('/api', cloudinaryroutes);
+
+module.exports = app;
